847109e03563ff71e33f50d5f90c99468686cf24
chatcahda <chatchada_sookmee@hotmail.com>
2018-04-25 21:33:13 +0700

    client now can delete bear

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { deleteBear } from './actions';
 
 class App extends Component {
   constructor(props){
     super(props)
-    
   }
- 
   render() {
     let bears = this.props.bear;
     return (
       <div>
         {
-          bears.map(bear => <div key={bear.id}>{bear.name}</div>)
+          bears.map(bear => (
+            <div key={bear.id}>
+              {bear.name}
+              <button onClick={() => this.props.deleteBear(bear.id)}>X</button>
+            </div>
+          ))
         }
       </div>
     );
   }
 }
-
 let mapStateToProps = (state) => (
   {bear: state.bear}
 )
-export default connect(mapStateToProps)(App);App;
+export default connect(mapStateToProps, {
+  deleteBear: deleteBear
+})(App);
