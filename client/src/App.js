10bcc8d26d398661b22c102ee7c31efca3f945ff
chatcahda <chatchada_sookmee@hotmail.com>
2018-04-25 21:53:14 +0700

    supprt create bear

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { deleteBear } from './actions';
+import { deleteBear, createBear } from './actions';
 
 class App extends Component {
   constructor(props){
     super(props)
+    this.state = {bearName: ''};
+  }
+  onBearNameChanged = (e) => {
+    this.setState({bearName: e.target.value});
+  }
+  toCreateBear = () => {
+    this.props.createBear({name: this.state.bearName});
+    this.setState({bearName: ''});
   }
   render() {
     let bears = this.props.bear;
@@ -18,13 +26,17 @@ class App extends Component {
             </div>
           ))
         }
+        <input value={this.state.bearName} onChange={this.onBearNameChanged}/>
+        <button onClick={this.toCreateBear}>Add</button>
       </div>
     );
   }
 }
+
 let mapStateToProps = (state) => (
   {bear: state.bear}
 )
 export default connect(mapStateToProps, {
-  deleteBear: deleteBear
+  deleteBear,
+  createBear,
 })(App);
--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,7 @@ var bears = [
     { id: '1', name: 'Ple' },
     { id: '2', name: 'Tum' }
 ]; 
+var last_bear_id = 3;
   
 router.route('/bears') 
     .get(function(req, res) {
@@ -16,15 +17,16 @@ router.route('/bears')
     .post(function(req, res) { 
         var bear = {}; 
         bear.name = req.body.name; 
+        bear.id = "" + (last_bear_id++);
         bears.push(bear); 
         res.json({ message: 'Bear created!' }); 
     })
-router.route('/bears/:id')    
+router.route('/bears/:id')
     .delete(function(req, res){
         bears = bears.filter(b => b.id !== req.params.id)
         res.json({ message: 'Bear deleted!' }); 
     }) 
-    
+
 app.use(cors());
 // all of our routes will be prefixed with /api 
 app.use('/api', bodyParser.json(), router); 
